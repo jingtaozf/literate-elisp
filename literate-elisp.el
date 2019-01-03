69af3f1fdaabf38178603deb32e233a2190e24da
Jingtao Xu <jingtaozf@gmail.com>
2019-01-03 15:49:47 +0800

    fix typo.

--- literate-elisp.el
+++ literate-elisp.el
@@ -24,7 +24,7 @@
 
 ;;; Commentary:
 
-;; Literate-elisp is an Emacs Lisp library to provide an easy way to use literal programming in Emacs Lisp.
+;; Literate-elisp is an Emacs Lisp library to provide an easy way to use literate programming in Emacs Lisp.
 ;; It extends the Emacs load mechanism so Emacs can load Org files as Lisp source files directly.
 
 ;;; Code:
